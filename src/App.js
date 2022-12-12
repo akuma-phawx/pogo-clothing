14af1b8cdcfd305b626495f473469baddbb44cdb
Christoforos Vradis <chrisvradis00@gmail.com>
2022-12-12 18:56:24 +0200

    feat(firebase): add firebas google auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Shop from './routes/shop/shop.component';
 import { Routes, Route } from 'react-router-dom';
+import SignIn from './routes/signin/signin.component';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<Shop />} />
+        <Route path="/sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
